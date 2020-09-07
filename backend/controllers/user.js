36ddd75ea356b5f1b2ce42a773f1d6287152f585
Borsas <borsasyt@gmail.com>
2020-09-07 22:39:09 +0300

    Created ability to get users oinks

---
+++ backend/controllers/user.js
@@ -0,0 +1,32 @@
+const userRouter = require("express").Router()
+
+const User = require("../database/models/User")
+const Oink = require("../database/models/Oink")
+
+userRouter.get("/:id", async (req, res) => {
+    const userId = req.params.id 
+
+    const user = await User.findByPk(userId)
+
+    if (!user) {
+        res.status(401).json({
+            error: "Invalid user id"
+            
+        })
+    }
+
+    const oinks = await Oink.findAll({
+        attributes: [
+            "username",
+            "content",
+            "date",
+            "likes"
+        ],
+        where: {
+            userId: userId
+        }
+    })
+    res.json(oinks)
+})
+
+module.exports = userRouter
